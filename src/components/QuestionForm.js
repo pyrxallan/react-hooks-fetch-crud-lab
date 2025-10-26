b9c9766f93edd9eb971f8588cc10afac12f87e2c
Allan Ratemo <allanisratemo@gmail.com>
2025-10-26 14:19:43 +0300

    Add proper form submission

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -19,7 +19,20 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    
+    // Format data for API
+    const questionData = {
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4,
+      ],
+      correctIndex: parseInt(formData.correctIndex),
+    };
+
+    props.onAddQuestion(questionData);
   }
 
   return (
